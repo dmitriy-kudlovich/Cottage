8ceac86ac91f322634593b8f82abc6f159385f13
Dmitriy Kudlovich <carnagery@mail.ru>
2023-05-03 21:14:55 +0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const navMobile = document.querySelector("#navMobile");
+const btnClose = document.querySelector("#btnClose");
+const overlay = document.querySelector("#overlay");
+const body = document.querySelector("body");
+let linksMobile = document.querySelectorAll(".nav__row--mobile .nav__link");
 const formSubmit = document.querySelector("#formSubmit");
 
 formSubmit.addEventListener("submit", (event) => {
   event.preventDefault();
 });
+
+btnHamburger.addEventListener("click", () => {
+  navMobile.classList.add("header__nav--mobile--active");
+  overlay.classList.add("overlay--active");
+  body.classList.add("overflow-hidden");
+});
+
+btnClose.addEventListener("click", closeNavMobile);
+
+navMobile.addEventListener("click", (event) => {
+  if (event.target.classList.contains("nav__link")) {
+    closeNavMobile();
+  }
+});
+
+function closeNavMobile() {
+  navMobile.classList.remove("header__nav--mobile--active");
+  overlay.classList.remove("overlay--active");
+  body.classList.remove("overflow-hidden");
+}
